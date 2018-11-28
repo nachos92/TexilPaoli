a5c7f3e44680bdd358dc9f0aa2f8546d341aa2a6
Nico <piano92@hotmail.it>
2018-11-28 19:46:30 +0100

    Bozza del javascript Init

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+var chiSiamo = "Prova per vedere se funziona l'Init()."
+
+
+function Init() {
+    /**
+     * Metto i testi nei relativi elementi.
+     */
+    jQuery('#chiSiamo').text(chiSiamo);
+    console.log("Init");
+
+}
