0067a6aff64589495fa6f52009c00ce4a4c2dcf7
Nico <piano92@hotmail.it>
2018-11-28 21:46:33 +0100

    Aggiunte delle stringhe (bozza)

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,15 @@
-var chiSiamo = "Prova per vedere se funziona l'Init()."
+var chiSiamo = "\
+        TEXIL PAOLI SRL OPERA NEL SETTORE DEL COMMERCIO DI TESSUTI \
+        E ABBIGLIAMENTO DI  STOCK DA OLTRE 30 ANNI. IL NOSTRO CORE BUSINESS È IL RITIRO \
+        E LA COMMERCIALIZZAZIONE DI RIMANENZE DI PRODOTTO (TESSUTI DI STOCK ABBIGLIAMENTO DI STOCK ) \
+        DI FAMOSI BRAND ITALIANI.";
+var sez_chiSiamo = ""
+//TODO Inserire stringona qui o spostare tutte le stringhe in un altro file.
+
+$(document).ready(function () {
+    Init();
+}
+);
 
 
 function Init() {
@@ -7,5 +18,10 @@ function Init() {
      */
     jQuery('#chiSiamo').text(chiSiamo);
     console.log("Init");
+
+}
+
+
+function getTesti() {
 
 }
